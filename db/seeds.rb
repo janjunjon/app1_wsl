a7abcf1483dfba2203e259fc2e7dc17909c794a1
janjunjon <jun.mr.children@gmail.com>
2021-03-08 14:38:27 +0000

    add top, article/index,article/show

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,51 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Article.create!(title: "Differences of atmospheric fields between years with heavy rain and little rain over Kyushu region",
+				author: "Juntaro ISHIHARA",
+				year: 2020,
+				abstract: "Heavy rain accompanied by Baiu front is often 
+						observed over Kyushu region in Baiu season. 
+						In addition, a damage with this phenomenon 
+						at the same time is quite serious. Thus, 
+						there are many researches about rainfall 
+						in Baiu season. In this paper, 
+						by using average precipitation of each year from 2000 
+						to 2019 we quantitatively captured, 
+						each characteristics of years with heavy rain 
+						and little rain. Top 5 years are defined as years 
+						with heavy rain, by choosing 5 years in the highest 
+						order. In contrast, Bottom 5 years are defined 
+						as years with little rain, by choosing 5 years 
+						in the lowest order. As a result, in years 
+						with heavy rain, amounts of water vapor over 
+						Kyushu and that carried to Kyushu region are 
+						more than normal year. In opposition, amounts 
+						of water vapor over Kyushu and surrounding areas, 
+						and that carried to Kyushu region are less than 
+						normal year in years with little rain. Moreover, 
+						there was a statistical difference between years 
+						with heavy rain and years with little rain in water 
+						vapor flux divergence, and we revealed that 
+						precipitation depends on water vapor flux divergence. 
+						Besides, sea surface temperature of both years with 
+						heavy rain and little rain from east China sea to 
+						off Kyushu region was higher than normal year. 
+						In contrast to this, sea surface temperature of 
+						both years with heavy rain and little rain was 
+						lower over south of east China sea. From a view 
+						point of relative vorticity, a disturbance over 
+						north off Kyushu was stronger in years with heavy 
+						rain, and high pressure over south off Kyushu was stronger 
+						in years with little rain.")
+
 30.times do |n|
 	title = "test ${n}"
-	name = Faker::Name.name
+	author = Faker::Name.name
 	year = rand(2000..2020)
-	abstract = Faker::Lorem.sentence(wordcount: 100)
+	abstract = Faker::Lorem.sentence(word_count: 100)
 	Article.create!(title: title,
-					name: name,
+					author: author,
 					year: year,
 					abstract: abstract,
 					created_at: Time.zone.now,
