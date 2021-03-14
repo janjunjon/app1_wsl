8a4e4c3ffbe0b0d57411ae3e56d76c4d979aac24
janjunjon <jun.mr.children@gmail.com>
2021-03-14 10:08:54 +0000

    add users/edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action: logged_in_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
   end
@@ -18,9 +20,11 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find_by(id: params[:id])
   end
 
   def update
+    @user = User.update(user_params)
   end
 
   def destroy
