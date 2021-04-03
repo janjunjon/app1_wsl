a040935b1bf325cddef4c1c3547b290e48adf02b
janjunjon <jun.mr.children@gmail.com>
2021-04-03 09:56:21 +0000

    edit search form/parameters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,21 @@
 class ArticlesController < ApplicationController
+	before_action :logged_in_user, only: :show
+
 	def index
-		@articles = Article.all.paginate(page: params[:page], per_page: 10)
+		if params[:degree] == nil || params[:keyword] == nil
+			@articles = Article.all.paginate(page: params[:page], per_page: 10)
+		else
+			@articles = Article.where('abstract like ?', "%#{params[:keyword]}%").paginate(page: params[:page], per_page: 10)
+			# @articles = Article.where(degree: params[:degree], abstract: "%#{params[:keyword]}%", year: params[:year]).paginate(page: params[:page], per_page: 10)
+		end		
 	end
 
 	def show
 		@article = Article.find_by(id: params[:id])
 	end
+
+	private
+		def article_params
+			params.require(:articles).permit(:degree, :submit)
+		end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
         session[:user_id] = user.id
     end
 
+    def logged_in_user
+        if session[:user_id] == nil
+          redirect_to login_path
+        end
+    end
+
     def current_user
         if session[:user_id]
             @current_user = User.find_by(id: session[:user_id])
