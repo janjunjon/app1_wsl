2328c6065d474d599ba507c10e48d8e2d6072509
janjunjon <jun.mr.children@gmail.com>
2021-04-03 14:46:23 +0000

    add case dividing for search

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,16 @@
 class ArticlesController < ApplicationController
+	include ArticlesHelper
 	before_action :logged_in_user, only: :show
 
 	def index
-		if params[:degree] == nil || params[:keyword] == nil
+		if params[:degree].nil? && params[:keyword].nil? && params[:year].nil?
 			@articles = Article.all.paginate(page: params[:page], per_page: 10)
 		else
-			@articles = Article.where('abstract like ?', "%#{params[:keyword]}%").paginate(page: params[:page], per_page: 10)
-			# @articles = Article.where(degree: params[:degree], abstract: "%#{params[:keyword]}%", year: params[:year]).paginate(page: params[:page], per_page: 10)
+			search_articles
 		end		
 	end
 
 	def show
 		@article = Article.find_by(id: params[:id])
 	end
-
-	private
-		def article_params
-			params.require(:articles).permit(:degree, :submit)
-		end
 end
