c230092b89c6feb4837df494b6561c20846bd8f2
janjunjon <jun.mr.children@gmail.com>
2021-04-12 09:51:21 +0000

    edit lagis authentication

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -5,10 +5,10 @@ class CodesController < ApplicationController
   def post
     @code = Code.find(1)
     if @code.authenticate(params[:code][:password])
-      cookies.permanent.signed[:id] = 1
-      cookies.permanent[:lagis_autentication] = params[:code][:password]
-      # session[:id] = 1
-      # session[:lagis_autentication] = params[:code][:password]
+      # cookies.permanent.signed[:id] = 1
+      # cookies.permanent[:lagis_autentication] = params[:code][:password]
+      session[:id] = 1
+      session[:lagis_autentication] = params[:code][:password]
       redirect_to root_path
       flash[:success] = "Lagisの認証コードが確認できました。"
     else
