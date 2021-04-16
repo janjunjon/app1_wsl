be0c721b1f4a94cb4580a89b333515b86677427e
janjunjon <jun.mr.children@gmail.com>
2021-04-16 08:25:40 +0000

    make user icon image uploader completely

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,11 @@ class User < ApplicationRecord
 	valid_format = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
 	validates :email, presence: true, length: { maximum: 200 }, format: { with: valid_format}, uniqueness: true
 	validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+	validates :image, content_type: { in: %w[image/jpeg image/gif image/png image/svg], message: "フォーマットが対応していません。"},
+					  size: { less_than: 5.megabytes, message: "ファイルサイズが大きすぎます。" }
 	has_secure_password
 	has_one_attached :image
+	# mount_uploader :image, ImageUploader
 
 	def self.mk_token
 		SecureRandom.urlsafe_base64
@@ -27,5 +30,10 @@ class User < ApplicationRecord
 
 	def authenticated?(token)
 		BCrypt::Password.new(remember_digest).is_password?(token)
+	end
+
+	def resized_image
+		self.image.variant(gravity: :center, resize:"80x80^", crop:"80x80+0+0").processed
+		# image.variant(resize_to_limit: [500, 500])
 	end
 end
